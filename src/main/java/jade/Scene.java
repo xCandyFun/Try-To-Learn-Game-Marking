3b89eb7632fc17e1d53d9073d4bfc2392eb3f8df
Simon T <simtho0624@gmail.com>
2024-05-12 12:44:15 +0200

    A bit more about shaders

---
+++ src/main/java/jade/Scene.java
@@ -0,0 +1,15 @@
+package jade;
+
+public abstract class Scene {
+
+    protected Camera camera;
+
+    public Scene(){
+
+    }
+    public void init(){
+
+    }
+
+    public abstract void update(float dt);
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+
+    public static void main(String[] args) {
+        jade.Window window = jade.Window.get();
+        window.run();
+    }
+}
